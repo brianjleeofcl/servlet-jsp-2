db2c9d0033b9d767741a09dc68bfc53753e2dced
Brian Lee <brianjleeofcl@gmail.com>
2017-09-28 18:13:52 -0700

    Add user servlet without functionality

---
+++ src/main/java/net/brianjlee/servletjsp2/UserServlet.java
@@ -0,0 +1,24 @@
+package net.brianjlee.servletjsp2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "userServlet", urlPatterns = { "users" }, loadOnStartup = 1)
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        PrintWriter writer = resp.getWriter();
+        writer.println("ok");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
